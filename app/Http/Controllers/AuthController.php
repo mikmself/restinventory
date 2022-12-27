2d07c1e9e340f04584d27b5ddd9a84561e7dd612
mikmself <mikmself@gmail.com>
2022-12-27 16:47:12 +0700

    change credential option

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Karyawan;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -43,11 +42,33 @@ class AuthController extends Controller
                     return $this->respondWithToken($token, $firstname,$unitkerja ,$lastname, $nip, $email, $acToken);
                 }
             } else {
-                return response()->json([
-                    'code' => 0,
-                    'message' => 'email anda tidak ditemukan!',
-                    'data' => []
-                ]);
+                $user2 = User::where('email',$request->nip)->first();
+                if(isset($user2)){
+                    $firstname = $user2->firstname;
+                    $lastname = $user2->lastname;
+                    $nip = $user2->nip;
+                    $email = $user2->email;
+                    $unitkerja = $user2->unitkerja->nama;
+                    if (!$token = Auth::attempt($credentials)) {
+                        return response()->json([
+                            'code' => 0,
+                            'message' => 'Unauthorized',
+                            'data' => []
+                        ], 401);
+                    } else {
+                        $user2->update([
+                            'token' => Str::random(60)
+                        ]);
+                        $acToken = $user2->token;
+                        return $this->respondWithToken($token, $firstname,$unitkerja ,$lastname, $nip, $email, $acToken);
+                    }
+                }else{
+                    return response()->json([
+                        'code' => 0,
+                        'message' => 'nip/email anda tidak ditemukan!',
+                        'data' => []
+                    ]);
+                }
             }
         }
     }
